4e4cb58edebea1f4bf2baa2270878f9a8c99593d
Adrian Flores Parraga <aflores-@c1r11s5.42madrid.com>
2026-08-23 20:44:28 +0200

    god

--- Cub3d/cub3d.h
+++ Cub3d/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jugarcia <jugarcia@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: aflores- <aflores-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/27 00:00:00 by                   #+#    #+#             */
-/*   Updated: 2026/08/18 12:24:06 by jugarcia         ###   ########.fr       */
+/*   Updated: 2026/08/23 20:25:19 by aflores-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -134,64 +134,64 @@ typedef struct s_texdraw
 }	t_texdraw;
 
 /* Funciones de cierre y eventos */
-int		close_window(t_data *data);
-int		key_press(int keycode, t_data *data);
-int		key_release(int keycode, t_data *data);
-int		render_frame(t_data *data);
+int			close_window(t_data *data);
+int			key_press(int keycode, t_data *data);
+int			key_release(int keycode, t_data *data);
+int			render_frame(t_data *data);
 
 /* Funciones de texturas */
-int		load_textures(t_data *data);
-void	free_textures(t_data *data);
+int			load_textures(t_data *data);
+void		free_textures(t_data *data);
 
 /* Funciones de parsing */
-int		parse_file(char *filename, t_data *data);
-void	free_map(t_map *map);
-void	print_error(char *message);
-int		init_player(t_data *data);
-int		check_file_extension(char *filename);
-void	init_map(t_map *map);
-int		is_map_line(char *line);
-int		parse_texture(char *line, t_map *map);
-int		parse_color(char *line, t_map *map);
-int		parse_line(char *line, t_map *map);
-int		read_map_grid(int fd, t_map *map, char *first_line);
-int		check_map_walls(t_map *map);
-int		validate_config(t_map *map);
+int			parse_file(char *filename, t_data *data);
+void		free_map(t_map *map);
+void		print_error(char *message);
+int			init_player(t_data *data);
+int			check_file_extension(char *filename);
+void		init_map(t_map *map);
+int			is_map_line(char *line);
+int			parse_texture(char *line, t_map *map);
+int			parse_color(char *line, t_map *map);
+int			parse_line(char *line, t_map *map);
+int			read_map_grid(int fd, t_map *map, char *first_line);
+int			check_map_walls(t_map *map);
+int			validate_config(t_map *map);
 
 /* Funciones de movimiento */
-void	move_forward_backward(t_data *data, int direction);
-void	move_strafe(t_data *data, int direction);
-void	rotate_camera(t_data *data, int direction);
-void	update_movement(t_data *data);
+void		move_forward_backward(t_data *data, int direction);
+void		move_strafe(t_data *data, int direction);
+void		rotate_camera(t_data *data, int direction);
+void		update_movement(t_data *data);
 
 /* Funciones de renderizado */
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
-int		get_texture_color(t_texture *tex, int x, int y);
-int	get_tex_x(t_texture *tex, t_wall *wall);
+void		my_mlx_pixel_put(t_data *data, int x, int y, int color);
+int			get_texture_color(t_texture *tex, int x, int y);
+int			get_tex_x(t_texture *tex, t_wall *wall);
 t_texture	*select_texture(t_data *data, int side, double ray_dir_x,
 				double ray_dir_y);
-void	draw_background(t_data *data);
-void	calculate_ray_direction(t_data *data, int x,
-			double *ray_dir_x, double *ray_dir_y);
-void	draw_wall_column(t_data *data, int x, t_wall *wall);
-void	render_walls(t_data *data);
-void	cast_ray(t_data *data, int x, t_ray *ray);
+void		draw_background(t_data *data);
+void		calculate_ray_direction(t_data *data, int x,
+				double *ray_dir_x, double *ray_dir_y);
+void		draw_wall_column(t_data *data, int x, t_wall *wall);
+void		render_walls(t_data *data);
+void		cast_ray(t_data *data, int x, t_ray *ray);
 
 /* Funciones auxiliares de string */
-int		ft_strlen(char *str);
-char	*ft_strdup(char *str);
-char	*ft_strtrim(char *str);
-int		ft_strncmp(char *s1, char *s2, int n);
-char	*get_next_line(int fd);
+int			ft_strlen(char *str);
+char		*ft_strdup(char *str);
+char		*ft_strtrim(char *str);
+int			ft_strncmp(char *s1, char *s2, int n);
+char		*get_next_line(int fd);
 
 /* Funciones auxiliares de iniciacion */
-int		init_game(t_data *data, char *file);
-int		init_mlx(t_data *data);
-void	init_hooks(t_data *data);
-void	cleanup(t_data *data);
+int			init_game(t_data *data, char *file);
+int			init_mlx(t_data *data);
+void		init_hooks(t_data *data);
+void		cleanup(t_data *data);
 
 /* Funciones auxiliares de raycasting */
-void	init_ray(t_data *data, int x, t_ray *ray);
-void	init_delta_dist(t_ray *ray);
+void		init_ray(t_data *data, int x, t_ray *ray);
+void		init_delta_dist(t_ray *ray);
 
 #endif
